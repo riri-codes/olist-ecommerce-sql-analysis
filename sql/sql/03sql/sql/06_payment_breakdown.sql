259c3d850bc1741d40a35f709f0b6163ea76db32
Rasika Gotkhindi <rasikagotkhindi2005@gmail.com>
2026-08-22 19:55:06 +0530

    Delete sql/sql/03sql/sql/06_payment_breakdown.sql

--- sql/sql/03sql/sql/06_payment_breakdown.sql
+++
@@ -1,8 +0,0 @@
--- Business question: What payment methods do customers use, and how do they compare in value and installment behavior?
-SELECT payment_type,
-       COUNT(*) AS num_payments,
-       ROUND(SUM(payment_value), 2) AS total_value,
-       ROUND(AVG(payment_installments), 1) AS avg_installments
-FROM olist_order_payments_dataset
-GROUP BY payment_type
-ORDER BY total_value DESC;
